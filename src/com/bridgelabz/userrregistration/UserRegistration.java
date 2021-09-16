9da0c13e7634bedfe6be28851165f1447f9cbb63
govind6498 <govindsingh6498@gmail.com>
2021-09-16 22:33:03 +0530

    Added:To check last name of first character is cap and min three character

--- src/com/bridgelabz/userrregistration/UserRegistration.java
+++ src/com/bridgelabz/userrregistration/UserRegistration.java
@@ -5,27 +5,32 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public static void checkValidFirstName(String firstName) {
+	public static void checkValidFirstName(String firstName,String lastName) {
 		boolean isFirstName;
-		String firstNameRegex = "[A-Z]{1}[a-z]{2,}";
-		Pattern patternObject = Pattern.compile(firstNameRegex);
-		if(firstName == null) {
+		String firstNameRegex = "[a-zA-Z] {1,}";
+		String lastNameRegex = "[A-Z]{1}[a-z]{2,}";
+		Pattern patternObject = Pattern.compile(lastNameRegex);
+		if(lastName == null) {
 			isFirstName = false;
 		}
-		Matcher matcherObject = patternObject.matcher(firstName);
+		Matcher matcherObject = patternObject.matcher(lastName);
 		isFirstName = matcherObject.matches();
 
 		if(isFirstName) {
-			System.out.println(firstName+" is an Valid First Name");
+			System.out.println(lastName+" is an Valid Last Name");
 			System.exit(0);
 		}
-		System.out.println(firstName+" is an Invaid First Name");
+		System.out.println(lastName+" is an Invaid Last Name");
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration ");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter First Name:");
 		String firstName = sc.next();
-		checkValidFirstName(firstName);
+
+		System.out.println("Enter First Name:");
+		String lastName = sc.next();
+		checkValidFirstName(firstName,lastName);
+		
 	}
 }
