415efdfbe2036042a857e6c13fe664e012204488
govind6498 <govindsingh6498@gmail.com>
2021-09-17 01:07:46 +0530

    REFECTOR:Method to check for valid Mobile Number

--- src/com/bridgelabz/userrregistration/UserRegistration.java
+++ src/com/bridgelabz/userrregistration/UserRegistration.java
@@ -5,71 +5,28 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public static void checkValidEmail(String email) {
-		boolean isEmail;
-		String validEmail="^[a-zA-z]+[+ . -]?[0-9]*[a-zA-Z]*[@]{1}[a-zA-Z]+[.]{1}[a-zA-Z]+[.]?[a-zA-Z]*$";
-		Pattern patternObject = Pattern.compile(validEmail);
-		if(email == null) {
-			isEmail = false;
+	public static void checkValidMObileNumber(String moNumber) {
+		boolean ismoNumber;
+		String validMoNumber="^[0-9]{2}\\s{1}[1-9]{1}[0-9]{9}";
+		Pattern patternObject = Pattern.compile(validMoNumber);
+		if(moNumber == null) {
+			ismoNumber = false;
 		}
-		Matcher matcherObject = patternObject.matcher(email);
-		isEmail = matcherObject.matches();
+		Matcher matcherObject = patternObject.matcher(moNumber);
+		ismoNumber = matcherObject.matches();
 
-		if(isEmail) {
-			System.out.println(email+" is an Valid Email");
+		if(ismoNumber) {
+			System.out.println(moNumber+" is an Valid MObile Number");
 		}
 		else {
-			System.out.println(email+" is an Invaid Email");
+			System.out.println(moNumber+" is an Invaid Mobile Number");
 		}
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration ");
-		String email = "govindsingh6498@gmail.com";
-		checkValidEmail(email);
-		email="abc@gmail.com";
-		checkValidEmail(email);
-		
-		email="abc@gmail.com.1a";
-		checkValidEmail(email);
-		email="abc@gmail.com.aa.au";
-		checkValidEmail(email);
-		email="abc-100@yahoo.com";
-		checkValidEmail(email);
-		email="abc.100@yahoo.com";
-		checkValidEmail(email);
-		email="abc111@abc.com";
-		checkValidEmail(email);
-		email="abc.100@abc.com.au";
-		checkValidEmail(email);
-		email="abc-100@abc.net";
-		checkValidEmail(email);
-		email="abc@gmail.com";
-		checkValidEmail(email);
-		email="abc@abc@gmail.com";
-		checkValidEmail(email);
-		email="abc.@gmail.com";
-		checkValidEmail(email);
-		email="abc..2002@gmail.com";
-		checkValidEmail(email);
-		email="abc@%*.com";
-		checkValidEmail(email);
-		email="abc()*@gmail.com";
-		checkValidEmail(email);
-		email=".abc@abc.com";
-		checkValidEmail(email);
-		email="abc123@.com.com";
-		checkValidEmail(email);
-		email="abc123@.com";
-		checkValidEmail(email);
-		email="abc+100@gmail.com ";
-		checkValidEmail(email);
-		email="abc123@gmail.a ";
-		checkValidEmail(email);
-		email="abc@.com.my";
-		checkValidEmail(email);
-		email="abc-100@yahoo.com";
-		checkValidEmail(email);
-		
-		
+		String moNumber = "91 9798364309";
+		checkValidMObileNumber(moNumber);
+		moNumber = "97 0587694523";
+		checkValidMObileNumber(moNumber);
 	}
 }
