3400bc4b8b7168f01cf1c130394acc08ed9712b8
govind6498 <govindsingh6498@gmail.com>
2021-09-17 01:49:03 +0530

    REFECTOR:Method to check if all sample email are vaild and invalid

--- src/com/bridgelabz/userrregistration/UserRegistration.java
+++ src/com/bridgelabz/userrregistration/UserRegistration.java
@@ -1,36 +1,100 @@
 package com.bridgelabz.userrregistration;
-
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public static void checkValidPasswordUpper(String passwordUpper) {
-		boolean isPasswordUpper;
-		String validPasswordUpper="^(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*([@#$%^&-+=()])*).{8,}$";
-		Pattern patternObject = Pattern.compile(validPasswordUpper);
-		if(passwordUpper == null) {
-			isPasswordUpper = false;
+	public static void checkValidEmail(String email) {
+		boolean isEmail;
+		String validEmail="^[a-zA-z]+[+ . -]?[0-9]*[a-zA-Z]*[@]{1}[a-zA-Z]+[.]{1}[a-zA-Z]+[.]?[a-zA-Z]*$";
+		Pattern patternObject = Pattern.compile(validEmail);
+		if(email == null) {
+			isEmail = false;
 		}
-		Matcher matcherObject = patternObject.matcher(passwordUpper);
-		isPasswordUpper = matcherObject.matches();
+		Matcher matcherObject = patternObject.matcher(email);
+		isEmail = matcherObject.matches();
 
-		if(isPasswordUpper) {
-			System.out.println(passwordUpper+" is an Valid Password");
+		if(isEmail) {
+			System.out.println(email+" is an Valid Email");
 		}
 		else {
-			System.out.println(passwordUpper+" is an Invaid Password");
+			System.out.println(email+" is an Invaid Email");
 		}
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration ");
-		String password = "akaaR#$@ou87";
-		checkValidPasswordUpper(password);
-		password= "hruwd%&*&%@";
-		checkValidPasswordUpper(password);
-		password= "hru98wd";
-		checkValidPasswordUpper(password);
-		password= "hruwd%&*&%@";
-		checkValidPasswordUpper(password);
+		
+		System.out.println("---------- Valid Emails ----------");
+		String email = "abc@bridgelabz.co.in";
+		checkValidEmail(email);
+
+		email = "abc@gmail.com.com";
+		checkValidEmail(email);
+
+		email = "abc@yahoo.com";
+		checkValidEmail(email);
+
+		email = "abc@1.com";
+		checkValidEmail(email);
+
+		email = "abc-100@yahoo.com";
+		checkValidEmail(email);
+
+		email = "abc.100@yahoo.com";
+		checkValidEmail(email);
+
+		email = "abc111@abc.com";
+		checkValidEmail(email);
+
+		email = "abc-100@abc.net";
+		checkValidEmail(email);
+
+		email = "abc.100@abc.com.au";
+		checkValidEmail(email);
+
+		email = "abc+100@gmail.com";
+		checkValidEmail(email);
+
+		System.out.println("---------------------------------------------------");
+
+		System.out.println("---------- Invalid Emails ----------");
+
+		email = ".abc@abc.com";
+		checkValidEmail(email);
+
+		email = "abc";
+		checkValidEmail(email);
+
+		email = "abc@.com.my";
+		checkValidEmail(email);
+
+		email = "abc@abc@gmail.com";
+		checkValidEmail(email);
+
+		email = "abc()*@gmail.com";
+		checkValidEmail(email);
+
+		email = "abc..2002@gmail.com";
+		checkValidEmail(email);
+
+		email = "abc.@gmail.com";
+		checkValidEmail(email);
+
+		email = "abc123@.com";
+		checkValidEmail(email);
+
+		email = "abc123@.com.com";
+		checkValidEmail(email);
+
+		email = "abc123@gmail.a";
+		checkValidEmail(email);
+
+		email = "abc@%*.com";
+		checkValidEmail(email);
+
+		email = "abc@gmail.com.1a";
+		checkValidEmail(email);
+
+		email = "abc@gmail.com.aa.au";
+		checkValidEmail(email);
 	}
 }
